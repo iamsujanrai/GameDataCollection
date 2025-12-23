1b6c4eb73fc9165f77d6207b154e8a6f7178290d
Rojin Bastola <rojinbastola@gmail.com>
2025-12-23 22:37:36 +0545

    Page updated

--- GameDataCollection/obj/Debug/net8.0/GameDataCollection.AssemblyInfo.cs
+++ GameDataCollection/obj/Debug/net8.0/GameDataCollection.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameDataCollection")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09d8893aba80f0f2b48d3b97fd0fed57711ef180")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+880eaab320d0a6c21fdfb166bb545613e8c2cc83")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameDataCollection")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameDataCollection")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
