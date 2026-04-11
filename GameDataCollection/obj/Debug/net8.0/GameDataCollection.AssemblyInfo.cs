ea38ff453af2024498047fcb3912e610424ba647
Rojin Bastola <rojinbastola@gmail.com>
2026-04-11 23:45:34 +0545

    spin view update

--- GameDataCollection/obj/Debug/net8.0/GameDataCollection.AssemblyInfo.cs
+++ GameDataCollection/obj/Debug/net8.0/GameDataCollection.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameDataCollection")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b40bc1b987a8d37eb2a54dc843d0265c55758729")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8c7e427f75fa5290abb5d8cbf705c3201b15d3c")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameDataCollection")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameDataCollection")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
